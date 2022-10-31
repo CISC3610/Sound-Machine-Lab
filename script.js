1c95e2c925a4e872e84e583c870605e1cc9ac7ba
Daniel <danielchabi97@gmail.com>
2022-10-31 16:23:51 -0400

    updated

---
+++ script.js
@@ -0,0 +1,18 @@
+const audio1 = new Audio();
+audio1.src = "click.mp3"
+
+const audio2 = new Audio();
+audio2.src = "gameOver.wav"
+
+const audio3 = new Audio();
+audio3.src = "bark.wav"
+
+const audio4 = new Audio();
+audio4.src = "alarm.wav"
+
+const audio5 = new Audio();
+audio5.src = "roar.wav"
+
+const audio6 = new Audio();
+audio6.src = "laugh.wav"
+
